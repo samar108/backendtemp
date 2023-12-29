9a495d611968ffa64b6b728d621029eabec01dbb
samarjeet dangi <samarjeetdangi86@gmail.com>
2023-12-29 19:11:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("Welcome to twitter");
+});
+
+app.get("/twitter/profile", (req, res) => {
+  res.send("welcome to your profile ");
+});
+
+app.get("/youtube", (req, res) => {
+  res.send("welcome to youtube");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
